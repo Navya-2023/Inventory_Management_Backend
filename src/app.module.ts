b7358bb9689944c370bb52e7cc7dfd1857d8d853
Navya@2023 <navyageorge1002@gmail.com>
2024-02-24 20:01:46 +0530

    Initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,39 @@
+// import { Module } from '@nestjs/common';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+
+// @Module({
+//   imports: [],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
+// export class AppModule {}
+
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { Product } from './typeorm/entities/product.entity';
+import { User } from './typeorm/entities/user.entity';
+import { ProductModule } from './product/product.module';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'Navya@2001',
+      database: 'inventory_management_application',
+      entities: [Product, User],
+      synchronize: true,
+    }),
+    ProductModule,
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/typeorm/entities/product.entity.ts
@@ -0,0 +1,20 @@
+
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  quantity: number;
+
+  @Column()
+  price: number;
+}
