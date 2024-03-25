e7f12c2e19a2424a2f7de4c05814feac4bc2d509
Navya@2023 <navyageorge1002@gmail.com>
2024-03-25 16:07:17 +0530

    Feat:updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,8 @@
+import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import * as dotenv from 'dotenv';
-import { ValidationPipe } from '@nestjs/common'
-/**
- * Function to bootstrap the NestJS application
- * It then loads the enviornment variables.
- * Then it creates an instance of the AppModule.
- * Start the application and listen for incoming HTTP requests on port 3000
- * Call the bootstrap function to start the application
- **/
+
 async function bootstrap() {
   dotenv.config()
   const app = await NestFactory.create(AppModule)
